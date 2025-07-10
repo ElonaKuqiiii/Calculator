130111665f7d19ca80045305e79d7736cfbe61ec
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-07-10 23:05:37 +0200

    logjika e funksioneve

--- src/main/java/test/Calculator.java
+++ src/main/java/test/Calculator.java
@@ -120,6 +120,72 @@ public class Calculator implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        for(int i=0;i<10;i++){
+            if(e.getSource()==numberButtons[i]){
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
+            if(e.getSource()==decButton){
+                textField.setText(textField.getText().concat("."));
+            }
+        if(e.getSource()==addButton){
+           num1=Double.parseDouble(textField.getText());
+           operator='+';
+           textField.setText("");
+        }
+        if(e.getSource()==subButton){
+            num1=Double.parseDouble(textField.getText());
+            operator='-';
+            textField.setText("");
+        }
+
+        if(e.getSource()==mulButton){
+            num1=Double.parseDouble(textField.getText());
+            operator='*';
+            textField.setText("");
+        }
+
+        if(e.getSource()==divButton){
+            num1=Double.parseDouble(textField.getText());
+            operator='/';
+            textField.setText("");
+        }
+
+        if(e.getSource()==equButton){
+            num2=Double.parseDouble(textField.getText());
+            switch (operator){
+                case '+':
+                    result=num1+num2;
+                    break;
+                case '-':
+                    result=num1-num2;
+                    break;
+                case '*':
+                    result=num1*num2;
+                    break;
+                case '/':
+                    result=num1/num2;
+                    break;
+            }
+            textField.setText(String.valueOf(result));
+            num1=result;
+        }
+        if(e.getSource()==clrButton){
+            textField.setText("");
+        }
+
+        if(e.getSource()==delButton){
+            String string=textField.getText();
+            textField.setText("");
+            for(int i=0;i<string.length()-1;i++){
+                textField.setText(textField.getText()+string.charAt(i));
+            }
+        }
+
+
+
+
+
 
 
     }
